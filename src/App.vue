0275b6f1479d22a2e26cdeed28ec4de53efccc7a
luan1307 <luanck22v7k514@vlvh.ctu.edu.vn>
2024-03-16 15:50:45 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 export default {};
 </script>
 
@@ -6,6 +6,29 @@ export default {};
   <h1>Hello, Vue.js!</h1>
 </template>
 
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style> -->
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+<template>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
 <style>
 .page {
   max-width: 400px;
